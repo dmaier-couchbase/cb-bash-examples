6e5465f569429a98e22de3a96dcaf2d108df1911
David Maier <david.maier@couchbase.com>
2016-06-01 17:56:32 +0200

    Optimized the handling of default arguments

--- scripts/default_args.bash
+++ scripts/default_args.bash
@@ -1,6 +1,24 @@
-if [ "$ADMIN_NAME" = "" ];then export ADMIN_NAME=couchbase;fi
-if [ "$ADMIN_PWD" = "" ];then export ADMIN_PWD=couchbase;fi
-if [ "$MASTER_NAME" = "" ];then export MASTER_NAME=$HOSTNAME;fi
-if [ "$RAM_SIZE" = "" ];then export RAM_SIZE=1024;fi
-if [ "$DATA_DIR" = "" ];then export DATA_DIR=/opt/couchbase/var/lib/data;fi
-if [ "$IDX_DIR" = "" ];then export IDX_DIR=/opt/couchbase/var/lib/idx;fi
+function export_arg {
+
+  if env | grep -q "^$1="
+  then
+	echo "Variable $1 is already set"
+  else
+	echo "Setting variable $1 to $2"
+	export $1=$2
+   
+  fi
+}
+
+function main {
+
+  export_arg ADMIN_NAME couchbase
+  export_arg ADMIN_PWD couchbase
+  export_arg MASTER_NAME $HOSTNAME
+  export_arg RAM_SIZE 1024
+  export_arg DATA_DIR /opt/couchbase/var/lib/data
+  export_arg IDX_DIR /opt/couchbase/var/lib/idx
+}
+
+main
+
